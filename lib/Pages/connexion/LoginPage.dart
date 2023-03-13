52af9b924b788d06110dc128225633013ed08fa4
HASSAN Adam <adamhassanpro@gmail.com>
2023-03-13 14:25:41 +0100

    style connexion + inscription (sans verif mdp*2)

--- lib/Pages/connexion/LoginPage.dart
+++ lib/Pages/connexion/LoginPage.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatefulWidget 
+{
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> 
+{
   final _formKey = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
@@ -14,13 +16,17 @@ class _LoginPageState extends State<LoginPage> {
 
   String _errorMessage = '';
 
-  Future<void> _signIn() async {
-    try {
+  Future<void> _signIn() async 
+  {
+    try 
+    {
       final email = _emailController.text.trim();
       final password = _passwordController.text.trim();
 
-      if (email.isEmpty || password.isEmpty) {
-        setState(() {
+      if (email.isEmpty || password.isEmpty) 
+      {
+        setState(() 
+        {
           _errorMessage = 'Veuillez remplir tous les champs';
         });
         return;
@@ -30,17 +36,23 @@ class _LoginPageState extends State<LoginPage> {
 
       // Si l'authentification réussit, aller à la page d'accueil
       Navigator.pushReplacementNamed(context, '/home');
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        setState(() {
+    } 
+    on FirebaseAuthException catch (e) 
+    {
+      if (e.code == 'user-not-found') 
+      {
+        setState(() 
+        {
           _errorMessage = 'Aucun utilisateur trouvé pour cet email';
         });
       } else if (e.code == 'wrong-password') {
-        setState(() {
+        setState(() 
+        {
           _errorMessage = 'Mauvais mot de passe';
         });
       } else {
-        setState(() {
+        setState(() 
+        {
           _errorMessage = 'Erreur de connexion: ${e.message}';
         });
       }
@@ -48,70 +60,241 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Connexion'),
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
+      theme: ThemeData
+      (
+        scaffoldBackgroundColor: const Color(0xFF1a2025),
+        fontFamily: 'Google Sans',
+        textTheme: const TextTheme
+        (
+          bodyText1: TextStyle
+          (
+            fontSize: 16.0,
+            color: Colors.white,
+          ),
+          bodyText2: TextStyle
+          (
+            fontSize: 16.0,
+            color: Colors.white,
+          ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
+        elevatedButtonTheme: ElevatedButtonThemeData
+        (
+          style: ElevatedButton.styleFrom
+          (
+            primary: const Color(0xFF1a2025),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Color(0xFF1e262c),
+          border: OutlineInputBorder(),
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: Scaffold
+      (
+          body: Center
+        (
+          child: Padding
+          (
+          padding: const EdgeInsets.fromLTRB(50.0, 100.0, 50.0, 50.0),
+
+          child: Form
+          (
+            
             key: _formKey,
-            child: Column(
+            child: Column
+            (
+              
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextFormField(
+              children: 
+              [
+                
+                 Container
+                 (
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text
+                  (
+                    'Bonjour !',
+                    style: const TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                Container
+                (
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: Text
+                  (
+                    'Veuillez vous connecter ou créer un nouveau compte pour utiliser l\'application.',
+                    style: const TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                TextFormField
+                (
                   controller: _emailController,
                   keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  decoration: InputDecoration
+                (
+                  labelText: "Email",
+                  labelStyle: TextStyle
+                  (
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
+                  filled: true,
+                  fillColor: Color(0xFF1e262c),
+                  border: OutlineInputBorder(
+                  ),
+                  focusedBorder: OutlineInputBorder
+                  (
+                    
+                    borderSide: BorderSide
+                    (
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder
+                  (
+                    borderSide: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                  style: const TextStyle
+                  (
+                    color: Colors.white,
+                  ),
+                  validator: (value) 
+                  {
+                    if (value!.isEmpty) 
+                    {
                       return 'Veuillez entrer votre email';
                     }
                     return null;
                   },
                 ),
-                SizedBox(height: 16.0),
-                TextFormField(
+                const SizedBox(height: 16.0),
+                TextFormField
+                (
+                  
                   controller: _passwordController,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: InputDecoration
+                  (
                     labelText: 'Mot de passe',
-                    border: OutlineInputBorder(),
+                    labelStyle: TextStyle
+                  (
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
                   ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
+                  filled: true,
+                  fillColor: Color(0xFF1e262c),
+                  border: OutlineInputBorder(
+                  ),
+                  focusedBorder: OutlineInputBorder
+                  (
+                    
+                    borderSide: BorderSide
+                    (
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder
+                  (
+                    borderSide: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                  ),
+                  style: const TextStyle
+                  (
+                    color: Colors.white,
+                  ),
+                  validator: (value) 
+                  {
+                    if (value!.isEmpty) 
+                    {
                       return 'Veuillez entrer votre mot de passe';
                     }
                     return null;
                   },
                 ),
-                SizedBox(height: 16.0),
-                ElevatedButton(
+                const SizedBox(height: 100.0),
+                ElevatedButton
+                (
                   onPressed: _signIn,
-                  child: Text('Connexion'),
+                  style: ElevatedButton.styleFrom
+                  (
+                    padding: const EdgeInsets.fromLTRB(1.0, 20.0, 1.0, 20.0),
+                    primary: Color(0xFF636AF6),
+                  ),
+                  child: Text('Se connecter'),
                 ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/inscription');
-                  },
-                  child: Text("S'inscrire"),
+                SizedBox(height: 20),
+               ElevatedButton
+               (
+                onPressed: () 
+                {
+                  Navigator.pushNamed(context, '/inscription');
+                },
+                child: Text
+                (
+                  
+                  "Créer un nouveau compte",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'GoogleSans-Medium',
+                  ),
                 ),
+                style: ElevatedButton.styleFrom
+                (
+                  padding: const EdgeInsets.fromLTRB(1.0, 20.0, 1.0, 20.0),
+
+                  primary: Color(0xFF1a2025),
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder
+                  (
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Color(0xFF636AF6), width: 2.0),
+                  ),
+                ),
+              ),
+
 
                 if (_errorMessage.isNotEmpty)
-                  Text(
+                  Text
+                  (
                     _errorMessage,
-                    style: TextStyle(
+                    style: TextStyle
+                    (
                       color: Colors.red,
                       fontSize: 16.0,
                     ),
                   ),
               ],
             ),
+          ),
           ),
         ),
       ),
--- lib/Pages/inscription/SignUpPage.dart
+++ lib/Pages/inscription/SignUpPage.dart
@@ -40,42 +40,155 @@ class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Inscription"),
-      ),
+      backgroundColor: Color(0xFF1a2025),
+      
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.fromLTRB(50.0, 100.0, 50.0, 50.0),
         child: Form(
           key: _formKey,
           child: Column(
             children: [
+               Container
+                 (
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text
+                  (
+                    'Inscription !',
+                    style: const TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+
+                Container
+                (
+                  padding: const EdgeInsets.symmetric(vertical: 30.0),
+                  child: Text
+                  (
+                    'Veuillez saisir ces différentes informations, afin que vos listes soient sauvegardées.',
+                    style: const TextStyle
+                    (
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               TextFormField(
                 keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(labelText: "Email"),
-                validator: (input) => !input!.contains('@') ? 'Entrez une adresse email valide' : null,
+                decoration: InputDecoration
+                (
+                  labelText: "Email",
+                  labelStyle: TextStyle
+                  (
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFF1e262c),
+                  border: OutlineInputBorder(
+                  ),
+                  focusedBorder: OutlineInputBorder
+                  (
+                    
+                    borderSide: BorderSide
+                    (
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder
+                  (
+                    borderSide: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
+                validator: (input) =>
+                    !input!.contains('@') ? 'Entrez une adresse email valide' : null,
                 onSaved: (input) => _email = input!,
               ),
+              SizedBox(height: 16.0),
               TextFormField(
-                decoration: InputDecoration(labelText: "Mot de passe"),
+                decoration: InputDecoration(
+                  labelText: "Mot de passe",
+                  labelStyle: TextStyle(
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xFF1e262c),
+                  border: OutlineInputBorder(
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white.withOpacity(0.5),
+                    ),
+                  ),
+                ),
                 obscureText: true,
-                validator: (input) => input!.length < 6 ? 'Le mot de passe doit contenir au moins 6 caractères' : null,
+                validator: (input) =>
+                    input!.length < 6 ? 'Le mot de passe doit contenir au moins 6 caractères' : null,
                 onSaved: (input) => _password = input!,
               ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
+              const SizedBox(height: 100.0),
+              ElevatedButton
+              (
                 onPressed: _submit,
-                child: Text("S'inscrire"),
+                  style: ElevatedButton.styleFrom
+                  (
+                    padding: const EdgeInsets.fromLTRB(122.5, 20.0, 122.5, 20.0),
+                    primary: Color(0xFF636AF6),
+                    shape: RoundedRectangleBorder
+                    (
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                child: Text (
+                  "S'inscrire",
+                   style: TextStyle(
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                  ),
               ),
+               SizedBox(height: 20),
               TextButton(
-  onPressed: () {
-    Navigator.pushNamed(context, '/connexion');
-  },
-  child: Text("Ce connecter"),
-),
-
-
-
-
+                onPressed: () {
+                  Navigator.pushNamed(context, '/connexion');
+                },
+                style: ElevatedButton.styleFrom
+                (
+                  padding: const EdgeInsets.fromLTRB(110.0, 20.0, 110.0, 20.0),
+                  primary: Color(0xFF1a2025),
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder
+                  (
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(color: Color(0xFF636AF6), width: 2.0),
+                  ),
+                ),
+                child: Text(
+                  "Se connecter",
+                  style: TextStyle(
+                    fontFamily: 'Google Sans',
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
 
             ],
           ),
@@ -83,4 +196,5 @@ class _SignUpPageState extends State<SignUpPage> {
       ),
     );
   }
+
 }
