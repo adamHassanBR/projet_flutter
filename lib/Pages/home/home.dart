23c6c4c26b812dee906b922fc08ae9f010133c3b
Faritaz <thibaultgautierl@gmail.com>
2023-03-20 16:34:30 +0100

    Ajout de la home page avec Connexion à L'API. Changer Initial Route en Connexion

--- lib/Pages/home/home.dart
+++ lib/Pages/home/home.dart
@@ -1,207 +1,311 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-  @override
-  _HomePageState createState() => _HomePageState();
+Future<List<int>> fetchGameIds() async {
+  final response = await http.get(Uri.parse('https://api.steampowered.com/ISteamChartsService/GetMostPlayedGames/v1/'));
+  
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> jsonResponse = json.decode(response.body);
+    final List<dynamic> ranks = jsonResponse['response']['ranks'];
+    final List<int> ids = ranks.map((rank) => rank['appid'] as int).toList();
+    return ids;
+  } else {
+    throw Exception('Failed to fetch game IDs');
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class Game {
+  final int id;
+  final String name;
+  final String imageUrl;
+
+  Game({required this.id, required this.name, required this.imageUrl});
+}
+
+Future<List<Game>> fetchGames(List<int> gameIds) async {
+  final List<Game> games = [];
+
+  for (int id in gameIds) {
+    final response = await http.get(Uri.parse('https://store.steampowered.com/api/appdetails?appids=$id'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic>? jsonResponse = json.decode(response.body)[id.toString()]['data'];
+      if (jsonResponse != null) {
+        final String name = jsonResponse['name'];
+        final String imageUrl = jsonResponse['header_image'];
+        final Game game = Game(id: id, name: name, imageUrl: imageUrl);
+        games.add(game);
+      }
+    } else {
+      throw Exception('Failed to fetch game details');
+    }
+  }
+
+  return games;
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _GameListPageState createState() => _GameListPageState();
+}
+
+class _GameListPageState extends State<HomePage> {
+  late Future<List<Game>> _futureGames;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureGames = _loadGames();
+  }
+
+  Future<List<Game>> _loadGames() async {
+    final gameIds = await fetchGameIds();
+    final games = await fetchGames(gameIds);
+    return games;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextField(
-          decoration: InputDecoration(
-            hintText: "Rechercher un jeu",
-            hintStyle: TextStyle(color: Colors.white),
-            prefixIcon: Icon(Icons.search),
-            filled: true,
-            fillColor: Color(0xFF1e262c),
-            border: InputBorder.none,
-          ),
-        ),
-        backgroundColor: Color(0xFF1e262c),
-        elevation: 0.0,
+        title: Text('Liste de jeux vidéo'),
       ),
-      backgroundColor: Color(0xFF1e262c),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 20.0,
-                left: 20.0,
-                bottom: 10.0,
-              ),
-              child: Text(
-                "Jeux les plus joués",
-                style: TextStyle(
-                  fontFamily: 'Google Sans',
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-            _buildMostPlayedGame(),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 20.0,
-                left: 20.0,
-                bottom: 10.0,
-              ),
-              child: Text(
-                "Tous les jeux",
-                style: TextStyle(
-                  fontFamily: 'Google Sans',
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-            _buildAllGames(),
-          ],
-        ),
+      body: FutureBuilder<List<Game>>(
+        future: _futureGames,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final games = snapshot.data!;
+            return ListView.builder(
+              itemCount: games.length,
+              itemBuilder: (context, index) {
+                final game = games[index];
+                return ListTile(
+                  leading: Image.network(game.imageUrl),
+                  title: Text(game.name),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('${snapshot.error}'),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
-
-  Widget _buildMostPlayedGame() {
-    return Container(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          // TODO: Add game image
-          SizedBox(height: 10.0),
-          Text(
-            "Titre du jeu",
-            style: TextStyle(
-              fontFamily: 'Google Sans',
-              color: Colors.white,
-              fontSize: 18.0,
-            ),
-          ),
-          SizedBox(height: 10.0),
-          Text(
-            "Courte description du jeu",
-            style: TextStyle(
-              fontFamily: 'Google Sans',
-              color: Colors.white.withOpacity(0.5),
-              fontSize: 14.0,
-            ),
-          ),
-          SizedBox(height: 20.0),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "En savoir plus",
-              style: TextStyle(
-                fontFamily: 'Google Sans',
-                color: Colors.white,
-              ),
-            ),
-            style: ElevatedButton.styleFrom(
-              primary: Color(0xFF636AF6),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-            ),
-          ),
-        ],
-      ),
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(
-              "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg?t=1628649045"),
-          fit: BoxFit.cover,
-        ),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-
-  Widget _buildAllGames() 
-  {
-    return Column
-    (
-      children: 
-      [
-        _buildGame("Nom du jeu 1", "\$9.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-        _buildGame("Nom du jeu 2", "\$19.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-        _buildGame("Nom du jeu 3", "\$29.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-        _buildGame("Nom du jeu 4", "\$39.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-        _buildGame("Nom du jeu 5", "\$49.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-        _buildGame("Nom du jeu 6", "\$59.99"),
-        Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
-      ],
-    );
-  }
-
-  Widget _buildGame(String title, String price) 
-  {
-    return InkWell
-    (
-      onTap: () 
-      {
-      // TODO: Implement on tap action
-      },
-    child: Container
-    (
-      color: Color(0xFF1e262c),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Row
-      (
-        children: 
-        [
-        Expanded
-        (
-          child: Column
-          (
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: 
-            [
-              Text
-              (
-                title,
-                style: TextStyle
-                (
-                  fontFamily: 'Google Sans',
-                  color: Colors.white,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text
-              (
-                price,
-                style: TextStyle
-                (
-                  fontFamily: 'Google Sans',
-                  color: Colors.white.withOpacity(0.5),
-                  fontSize: 14.0,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Icon
-        (
-          Icons.arrow_forward_ios_rounded,
-          color: Colors.white.withOpacity(0.5),
-        ),
-      ],
-    ),
-  ),
-);
 }
-}
+
+
+// import 'package:flutter/material.dart';
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: TextField(
+//           decoration: InputDecoration(
+//             hintText: "Rechercher un jeu",
+//             hintStyle: TextStyle(color: Colors.white),
+//             prefixIcon: Icon(Icons.search),
+//             filled: true,
+//             fillColor: Color(0xFF1e262c),
+//             border: InputBorder.none,
+//           ),
+//         ),
+//         backgroundColor: Color(0xFF1e262c),
+//         elevation: 0.0,
+//       ),
+//       backgroundColor: Color(0xFF1e262c),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Padding(
+//               padding: const EdgeInsets.only(
+//                 top: 20.0,
+//                 left: 20.0,
+//                 bottom: 10.0,
+//               ),
+//               child: Text(
+//                 "Jeux les plus joués",
+//                 style: TextStyle(
+//                   fontFamily: 'Google Sans',
+//                   color: Colors.white,
+//                   fontSize: 20.0,
+//                 ),
+//               ),
+//             ),
+//             _buildMostPlayedGame(),
+//             Padding(
+//               padding: const EdgeInsets.only(
+//                 top: 20.0,
+//                 left: 20.0,
+//                 bottom: 10.0,
+//               ),
+//               child: Text(
+//                 "Tous les jeux",
+//                 style: TextStyle(
+//                   fontFamily: 'Google Sans',
+//                   color: Colors.white,
+//                   fontSize: 20.0,
+//                 ),
+//               ),
+//             ),
+//             _buildAllGames(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Widget _buildMostPlayedGame() {
+//     return Container(
+//       padding: const EdgeInsets.all(20.0),
+//       child: Column(
+//         children: [
+//           // TODO: Add game image
+//           SizedBox(height: 10.0),
+//           Text(
+//             "Titre du jeu",
+//             style: TextStyle(
+//               fontFamily: 'Google Sans',
+//               color: Colors.white,
+//               fontSize: 18.0,
+//             ),
+//           ),
+//           SizedBox(height: 10.0),
+//           Text(
+//             "Courte description du jeu",
+//             style: TextStyle(
+//               fontFamily: 'Google Sans',
+//               color: Colors.white.withOpacity(0.5),
+//               fontSize: 14.0,
+//             ),
+//           ),
+//           SizedBox(height: 20.0),
+//           ElevatedButton(
+//             onPressed: () {},
+//             child: Text(
+//               "En savoir plus",
+//               style: TextStyle(
+//                 fontFamily: 'Google Sans',
+//                 color: Colors.white,
+//               ),
+//             ),
+//             style: ElevatedButton.styleFrom(
+//               primary: Color(0xFF636AF6),
+//               shape: RoundedRectangleBorder(
+//                 borderRadius: BorderRadius.circular(10.0),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//       decoration: BoxDecoration(
+//         image: DecorationImage(
+//           image: NetworkImage(
+//               "https://cdn.cloudflare.steamstatic.com/steam/apps/570/header.jpg?t=1628649045"),
+//           fit: BoxFit.cover,
+//         ),
+//         borderRadius: BorderRadius.circular(10.0),
+//       ),
+//     );
+//   }
+
+//   Widget _buildAllGames() 
+//   {
+//     return Column
+//     (
+//       children: 
+//       [
+//         _buildGame("Nom du jeu 1", "\$9.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//         _buildGame("Nom du jeu 2", "\$19.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//         _buildGame("Nom du jeu 3", "\$29.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//         _buildGame("Nom du jeu 4", "\$39.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//         _buildGame("Nom du jeu 5", "\$49.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//         _buildGame("Nom du jeu 6", "\$59.99"),
+//         Divider(height: 1.0, color: Colors.white.withOpacity(0.5)),
+//       ],
+//     );
+//   }
+
+//   Widget _buildGame(String title, String price) 
+//   {
+//     return InkWell
+//     (
+//       onTap: () 
+//       {
+//       // TODO: Implement on tap action
+//       },
+//     child: Container
+//     (
+//       color: Color(0xFF1e262c),
+//       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//       child: Row
+//       (
+//         children: 
+//         [
+//         Expanded
+//         (
+//           child: Column
+//           (
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: 
+//             [
+//               Text
+//               (
+//                 title,
+//                 style: TextStyle
+//                 (
+//                   fontFamily: 'Google Sans',
+//                   color: Colors.white,
+//                   fontSize: 18.0,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               Text
+//               (
+//                 price,
+//                 style: TextStyle
+//                 (
+//                   fontFamily: 'Google Sans',
+//                   color: Colors.white.withOpacity(0.5),
+//                   fontSize: 14.0,
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         Icon
+//         (
+//           Icons.arrow_forward_ios_rounded,
+//           color: Colors.white.withOpacity(0.5),
+//         ),
+//       ],
+//     ),
+//   ),
+// );
+// }
+// }
 
 
 
